367c0cc778486760bbb589d410694044e3e84c8e
jrick2 <joshrick16.abellera@gmail.com>
2023-04-06 05:02:47 -0500

    act DRDev

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (import.meta.env.VITE_RUN_BUILD) {
+  disableReactDevTools();
+}
 
 export const store = configureStore({
   reducer: { [api.reducerPath]: api.reducer },
